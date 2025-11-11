214efa736ac54d727b2df6980c634775c5e29578
gdKid <gdKid67@gmail.com>
2025-11-11 11:15:50 +0800

    Update PyOS Public Beta B1.1.3.py

--- PyOS Public Beta B1.1.3.py
+++ PyOS Public Beta B1.1.3.py
@@ -1,32 +1,39 @@
 import tkinter as tk
 import hashlib
+
 username = input('Enter your username: ')
 password = input('Enter your password: ')
+
 def New_Window():
     Window = tk.Toplevel()
     canvas = tk.Canvas()
     canvas.pack()
     print("Opened New profile with POS code 0S01")
+
 def New_Console():
     Window = tk.Toplevel()
     canvas = tk.Canvas(Window)
     canvas.pack()
     print("Opened New Console Window with POS code 0S02")
+    
 print("Opened PyOS Public beta version 1.1.3")
+
 root = tk.Tk()
 root.title("New Profile")
 root.minsize(298, 70)
 root.resizable(False, False)
+
 label1 = tk.Label(text= "Version PB1.1.3 (11-11-2025)")
 label1.pack()
 label2 = tk.Label(text= "By gdKid")
 label2.pack()
+
 button = tk.Button(root, text="New Profile", bg='White', fg='Black', command=lambda: New_Window())
 button.pack()
+
 buttonC = tk.Button(root, text="Console", bg='Black', fg='White', command=lambda: New_Console())
 buttonC.pack()
+
 root.mainloop()
 
 print("Ended Process on command (POS Code 0X01)")
-
-
